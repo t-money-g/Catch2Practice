ba236827cceb5318f9612d3c57b7c58db9ca7659
T <tyoungjr2005@gmail.com>
2021-10-31 16:23:15 -0500

    obligatory fizz buzz.

---
+++ src/math_problems.cpp
@@ -0,0 +1,31 @@
+//
+// Created by tyoun on 10/30/2021.
+//
+
+
+#include <catch2/catch.hpp>
+#include <iostream>
+
+using std::cout, std::endl;
+// write a program that calculates and prints the sum of all natural
+// numbers divisible by either 3 or 5 up to a given limit
+void print_three_and_five_divisibles( const int& limit) {
+
+    int i = 1;
+
+    while(i <= limit) {
+        if(i % 3 == 0)
+            cout << i << " is divisible by 3" << endl;
+        if(i % 5 == 0)
+            cout << i << " is divisible by 5" << endl;
+        i++;
+    }
+
+}
+
+TEST_CASE("print divisible numbers by 3 or 5 up to a limit") {
+
+    print_three_and_five_divisibles(100);
+}
+
+
